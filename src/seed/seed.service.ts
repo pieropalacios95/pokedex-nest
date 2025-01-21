ea055202065ff68c078617061669c88148d220aa
pieropalacios95 <palacios1595@outlook.com>
2025-01-21 16:59:26 -0500

    add pagination support and seed functionality; update README and package.json

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
+
+  async runSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=500',
+    );
+
+    const pokemonResponse = data.results.map(({ name, url }) => ({
+      name: name.toLocaleUpperCase(),
+      no: +url.split('/')[6],
+    }));
+
+    await this.pokemonModel.insertMany(pokemonResponse);
+
+    return 'Seed executed successfully';
+  }
+}
