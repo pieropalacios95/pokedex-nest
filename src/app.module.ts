44cbc1844d20f3bd3076ffa98b537453998b300a
pieropalacios95 <palacios1595@outlook.com>
2025-01-21 18:18:40 -0500

    update MongoDB connection to include database name from environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB,{dbName: process.env.MONGODB_NAME}),
     PokemonModule,
     CommonModule,
     SeedModule,
